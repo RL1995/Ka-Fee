3e03b2ad2e3b6216cf08812a5dc49b3a7d4cf000
RL1995 <119047856+RL1995@users.noreply.github.com>
2023-08-13 12:28:04 -0400

    first commit

---
+++ customers/views.py
@@ -0,0 +1,15 @@
+from customers.models import Customer
+from django.http.response import JsonResponse
+from customers.serializers import CustomerSerializer  
+
+def customers(request):
+    data = Customer.objects.all()
+    serializer = CustomerSerializer(data, many=True)  
+    return JsonResponse({'customers': serializer.data})
+
+
+
+def customer(request, id):
+    data = Customer.objects.get(pk=id)
+    serializer = CustomerSerializer(data)  
+    return JsonResponse({'customer': serializer.data})
